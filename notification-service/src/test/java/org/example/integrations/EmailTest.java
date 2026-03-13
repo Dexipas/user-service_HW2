17b9851c8c460e211c2c78595c1c8bd3c59fdf14
dexipas <sfdxyz@yandex.ru>
2026-03-13 11:33:47 +0300

    migrating services to docker

--- notification-service/src/test/java/org/example/integrations/EmailTest.java
+++ notification-service/src/test/java/org/example/integrations/EmailTest.java
@@ -5,17 +5,21 @@ import com.icegreen.greenmail.junit5.GreenMailExtension;
 import com.icegreen.greenmail.util.ServerSetupTest;
 import jakarta.mail.Message;
 import org.example.NotificationApp;
+import org.example.feign.UserClient;
 import org.example.repository.EventRepository;
 import org.example.service.NotificationService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.messaging.MessagingException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
 
 import java.io.IOException;
 
@@ -23,6 +27,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ContextConfiguration(classes = NotificationApp.class)
+
+@TestPropertySource(properties = {
+        "spring.liquibase.enabled=false",
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration",
+        "cron.expression=*/3 * * * * ?"
+})
 public class EmailTest {
     @RegisterExtension
     static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP)
@@ -42,8 +52,10 @@ public class EmailTest {
     private JavaMailSender javaMailSender;
     @Autowired
     private NotificationService notificationService;
-    @Autowired
+    @Mock
     private EventRepository eventRepository;
+    @Mock
+    private UserClient userClient;
 
 
     @Test
